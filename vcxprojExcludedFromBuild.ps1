6d28c82313b380ae8d91c7e83f4d4000840dcdde
Nicolas Martin <nicolas.martin@arobas-music.com>
2013-12-08 23:29:35 +0100

    ajout d'un script pour nettaoyer les exclude from build
    copi les exclude frombuild de la version Debug et les applique pour
    toutes les configurations

---
+++ vcxprojExcludedFromBuild.ps1
@@ -0,0 +1,34 @@
+﻿[xml]$vcroj = Get-Content "C:\dev\guitarpro7\Shared\guitarpro\msvc\guitarpro\bk.xml"
+$ns = new-object Xml.XmlNamespaceManager $vcroj.NameTable
+$ns.AddNamespace('n', 'http://schemas.microsoft.com/developer/msbuild/2003')
+$excludes = $vcroj.SelectNodes("//n:ExcludedFromBuild", $ns)
+
+#//'$(Configuration)|$(Platform)'=='Release|Win32'
+
+$clcompiles =  New-Object Collections.ArrayList
+$values =  New-Object Collections.ArrayList
+foreach($exclude in $excludes)
+{
+    if($exclude.Condition.Contains("Debug"))
+    {
+        
+        if($clcompiles.Contains($exclude.ParentNode))
+        {
+        }else
+        {
+            $values += $exclude.'#text';
+            $clcompiles += $exclude.ParentNode
+        }
+    }
+    $exclude.ParentNode.RemoveChild($exclude)
+}
+$i = 0
+foreach($cl in $clcompiles)
+{
+    $exclude = $vcroj.CreateElement('ExcludedFromBuild')
+    $text = $vcroj.CreateTextNode($values[$i])
+    $exclude.AppendChild($text)
+    $cl.AppendChild($exclude)
+    $i++
+}
+$vcroj.Save("c:\tmp\bk2.xml")
