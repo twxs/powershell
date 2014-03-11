e1c4efbab85ab9d894c3308111083dc2224eb185
Nicolas Martin <nicolas.martin@arobas-music.com>
2014-03-11 14:25:44 +0100

    charger un assembly

---
+++ reflexion.ps1
@@ -0,0 +1,5 @@
+﻿$gpkernel = [appdomain]::currentdomain.GetAssemblies() | where { $_ -and $_.Location -and $_.Location.Contains("gpkernel") }
+$gpkernel.GetExportedTypes() | % { $_.FullName }
+$app = New-Object [ArobasMusic.GuitarPro.GPApplicationWrapper] 
+
+Register-ObjectEvent -InputObject $app.StartEvent -EventName Executing -Action { Write-Host "<StartEvent>" }
